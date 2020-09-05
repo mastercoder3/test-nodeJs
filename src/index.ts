89cd0f8aed006817ef6f5b318c7481235370f153
Muhammad Junaid Hassan <muhammadjunaidhassan1@gmail.com>
2020-09-05 23:55:12 +0500

    all done

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
 import "reflect-metadata";
-import { GraphQLServer } from 'graphql-yoga'
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import { GraphQLServer } from 'graphql-yoga';
+// import { importSchema } from 'graphql-import';
+import { resolvers } from './resolver';
+
+
+// const typeDefs = importSchema('schema.graphql');
  
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
- 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name } : any) => `Hello ${name || 'World'}`,
-  },
-}
- 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs: 'src/schema.graphql', resolvers })
+server.start(() => console.log('Server is running on localhost:4000')) 
